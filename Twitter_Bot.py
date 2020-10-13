42dafe0596ec22a33a4b1a06f4bef9bb451c0e23
Hammms <35775868+Hammms@users.noreply.github.com>
2020-10-13 11:03:09 -0400

    Update Twitter_Bot.py

--- Twitter_Bot.py
+++ Twitter_Bot.py
@@ -1,7 +1,7 @@
 import tweepy
 import time 
 
-auth = tweepy.OAuthHandler('gwrB0lVHt6rilqMEZ3moqhw9B','vUyNmYn3VUtySREe0635n0WPCVFyiYwgPGIzps73RIUtCXXsA1')
+auth = tweepy.OAuthHandler('this is where the authkey goes')
 auth.set_access_token(#place your unique access token here)
 api = tweepy.API(auth)
 
@@ -34,7 +34,3 @@ for tweet in tweepy.Cursor(api.search, search).items(numoftweets):
         break
 
 
-#1239550280850649094-iWgIQUejrUthuO8oegB66e6qh96pZ1 Access token
-#5ggmVnuJBVB8NXr6yI33BiS3MjRhWZ0Vze0LqB9D9B4Im Acess token secret
-#gwrB0lVHt6rilqMEZ3moqhw9B API Key
-#vUyNmYn3VUtySREe0635n0WPCVFyiYwgPGIzps73RIUtCXXsA1 API Secret
